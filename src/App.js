12f43658e2c3caa4eceb56c7de6fbc295a0fc03d
Lucas Colombo <colombo.lucas@outlook.com>
2022-07-04 10:41:14 -0300

    routes and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/HomePage';
+import { AddNewPage } from './pages/AddNewPage';
+import { EditCardPage } from './pages/EditCardPage';
+import { EnterPage } from './pages/EnterPage';
+
 function App() {
-  return <div className="App"></div>;
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='/' element={<Home />}></Route>
+				<Route path='/add-concert' element={<AddNewPage />}></Route>
+				<Route path='/enter' element={<EnterPage />}></Route>
+				<Route path='/edit' element={<EditCardPage />}></Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
