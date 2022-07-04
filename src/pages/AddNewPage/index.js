4f0db7ce471bbde9df2502fb46e2467a176172cf
Lucas Colombo <colombo.lucas@outlook.com>
2022-07-04 14:59:36 -0300

    form inputs

--- src/pages/AddNewPage/index.js
+++ src/pages/AddNewPage/index.js
@@ -1,1 +1,18 @@
-export function AddNewPage() {}
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import InputLabel from '@mui/material/InputLabel';
+
+export function AddNewPage() {
+	return (
+		<>
+			<form>
+				<TextField
+					id='outlined-basic'
+					label='Concert/Tour'
+					variant='outlined'
+				/>
+				<TextField id='outlined-basic' label='Artist' variant='outlined' />
+			</form>
+		</>
+	);
+}
