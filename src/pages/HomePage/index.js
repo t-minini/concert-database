933afea76bc4cd29bc60dcf0489e2f473fe5267e
Lucas Colombo <colombo.lucas@outlook.com>
2022-07-05 16:27:10 -0300

    navbar

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,18 +1,32 @@
-import { ConcertCard } from "../../components/ConcertCard";
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
+import { ConcertCard } from '../../components/ConcertCard';
+import { Link } from 'react-router-dom';
+
+//Material UI
+import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 export function Home() {
-  return (
-    <>
-      <h1>CONCERT'S DATABASE</h1>
-      <h3>HOME</h3>
-      <ConcertCard />
-      <Link to={"/add-new"} style={{ textDecoration: "none" }}>
-        <Button size="small" variant="contained" color="success">
-          ADD NEW
-        </Button>
-      </Link>
-    </>
-  );
+	return (
+		<>
+			<Box sx={{ flexGrow: 1 }}>
+				<AppBar position='static'>
+					<Toolbar>
+						<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+							<strong>Home</strong>
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</Box>
+			<h1>CONCERT'S DATABASE</h1>
+			<ConcertCard />
+			<Link to={'/add-new'} style={{ textDecoration: 'none' }}>
+				<Button size='small' variant='contained' color='success'>
+					ADD NEW
+				</Button>
+			</Link>
+		</>
+	);
 }
